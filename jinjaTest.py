990fe41a686f9165921315ff5610dea14bb4574c
Jeff Glass <glass.jeffrey@gmail.com>
2025-01-31 08:52:42 -0600

    Delete old jinja experiment

--- jinjaTest.py
+++
@@ -1,9 +0,0 @@
-from jinja2 import Environment, PackageLoader, select_autoescape
-from flask import Flask, url_for
-
-env = Environment(
-	loader=PackageLoader('testApplication', 'templates'),
-	autoescape=select_autoescape(['html', 'xml'])
-	)
-
-env.globals={'url_for': url_for}
